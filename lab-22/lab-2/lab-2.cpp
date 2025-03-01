28decdc2781e5d1b39816e438b8b56bcedfaeae0
issakeR-33 <f60.c1tyx.tony@gmail.com>
2025-03-01 20:18:56 +0300

    add third class and lab completed!

--- lab-22/lab-2/lab-2.cpp
+++ lab-22/lab-2/lab-2.cpp
@@ -1,4 +1,5 @@
 ﻿#include<iostream>
+#include <windows.h>
 #include<string>
 
 using namespace std;
@@ -56,16 +57,52 @@ public:
 
 };
 
+class Menu {
+private:
+    string nameSnack;
+    string sizeSnack;
+    double priceSnack;
+    string nameDrink;
+    double sizeDrink;
+    double priceDrink;
+    //double amount;
+
+public:
+
+    Menu() : nameSnack(""), sizeSnack(""), priceSnack(0), nameDrink(""), sizeDrink(0), priceDrink(0) {};
+    Menu(string nS) : Menu(nS, "", 0, "", 0, 0) {};
+    Menu(string nS, string sS) : Menu(nS, sS, 0, "", 0, 0) {};
+    Menu(string nS, string sS, double pS) : Menu(nS, sS, pS, "", 0, 0) {};
+    Menu(string nS, string sS, double pS, string nD) : Menu(nS, sS, pS, nD, 0, 0) {};
+    Menu(string nS, string sS, double pS, string nD, double sD) : Menu(nS, sS, pS, nD, sD, 0) {};
+    Menu(string nS, string sS, double pS, string nD, double sD, double pD) : nameSnack(nS), sizeSnack(sS), priceSnack(pS), nameDrink(nD), sizeDrink(sD), priceDrink(pD) {};
+
+    void order() {
+        cout << "Snack: " << nameSnack << ". " << "Size " << sizeSnack << ". " << "Price: " << priceSnack << "grn." << endl;
+        cout << "Drink: " << nameDrink << ". " << "Size: " << sizeDrink << " " << "Price: " << priceDrink << "grn." << endl;
+        cout << "In total: " << priceSnack + priceDrink << "." << endl;
+
+    }
+    ~Menu() {
+        cout << "Order was deleted. " << endl;
+    }
+};
+
 
 int main() {
-    Movie_Session test("Fight Club", "21:00", "29.02.2025", 50);
-    test.film_info();
+    SetConsoleOutputCP(CP_UTF8);
+    Movie_Session Film1("Fight Club", "21:00", "29.02.2025", 50);
+    Film1.film_info();
 
     cout << endl;
 
-    Reserve_Tickets test2("Vanya'", "Fight club", "29.02.2025", "21:00", 5, 17);
-    test2.info_about_reserve();
+    Reserve_Tickets reserve1("Vanya'", "Fight club", "29.02.2025", "21:00", 5, 17);
+    reserve1.info_about_reserve();
 
-    cout  << endl;
-   
-}
+    cout << endl;
+
+    Menu order1("Cheese Pop-Koяn", "Medium", 150, "Cola", 0.3, 34.90);
+    order1.order();
+
+    cout << endl;
+}   
